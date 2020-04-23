0f784ef65f020df6a2ad39bb5e86b8e663d6b584
Alfonso Morales <Alfonso@neoware.io>
2020-04-23 10:48:46 -0400

    Initial ThumbsUp Commit
    This contains the intial project structure and styling. Included are screens as seen in the design file, which can be view here: https://zpl.io/agJAOZN

---
+++ App.js
@@ -0,0 +1,103 @@
+//
+//  App.js
+//  ThumbsUp UI_Ux
+//
+//  Created by [Author].
+//  Copyright © 2018 [Company]. All rights reserved.
+//
+
+import * as Font from "expo-font"
+import CardInformation from "./App/CardInformation/CardInformation"
+import CompanyHome from "./App/CompanyHome/CompanyHome"
+import CompanyPreview from "./App/CompanyPreview/CompanyPreview"
+import CompanySettings from "./App/CompanySettings/CompanySettings"
+import CompanySignupCompletion from "./App/CompanySignupCompletion/CompanySignupCompletion"
+import CreationPreview from "./App/CreationPreview/CreationPreview"
+import ImageUplaod from "./App/ImageUplaod/ImageUplaod"
+import React from "react"
+import SendPost from "./App/SendPost/SendPost"
+import SignupLogin from "./App/SignupLogin/SignupLogin"
+import TemplateSelection from "./App/TemplateSelection/TemplateSelection"
+import UserSettings from "./App/UserSettings/UserSettings"
+import UserSignupCompletion from "./App/UserSignupCompletion/UserSignupCompletion"
+import UserThankYou from "./App/UserThankYou/UserThankYou"
+import { AppLoading, DangerZone } from "expo"
+import { createAppContainer, createStackNavigator } from "react-navigation"
+
+const PushRouteOne = createStackNavigator({
+	SignupLogin: {
+		screen: SignupLogin,
+	},
+	CompanySignupCompletion: {
+		screen: CompanySignupCompletion,
+	},
+	CardInformation: {
+		screen: CardInformation,
+	},
+	CompanyHome: {
+		screen: CompanyHome,
+	},
+	TemplateSelection: {
+		screen: TemplateSelection,
+	},
+	ImageUplaod: {
+		screen: ImageUplaod,
+	},
+	CreationPreview: {
+		screen: CreationPreview,
+	},
+	SendPost: {
+		screen: SendPost,
+	},
+	UserThankYou: {
+		screen: UserThankYou,
+	},
+}, {
+	initialRouteName: "ImageUplaod",
+})
+
+const RootNavigator = createStackNavigator({
+	PushRouteOne: {
+		screen: PushRouteOne,
+	},
+}, {
+	mode: "modal",
+	headerMode: "none",
+	initialRouteName: "PushRouteOne",
+})
+
+const AppContainer = createAppContainer(RootNavigator)
+
+
+
+export default class App extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			fontsReady: false,
+		}
+	}
+
+	componentDidMount() {
+
+		this.initProjectFonts()
+	}
+
+	async initProjectFonts() {
+
+		await Font.loadAsync({
+			".AppleSystemUIFont": require("./assets/fonts/SFNS.ttf"),
+			"SFProDisplay-Regular": require("./assets/fonts/SFProDisplayRegular.otf"),
+		})
+		this.setState({
+			fontsReady: true,
+		})
+	}
+
+	render() {
+
+		if (!this.state.fontsReady) { return (<AppLoading />); }
+		return <AppContainer/>
+	}
+}
---
+++ ios/Pods/Target Support Files/Pods-ThumbsUp/Pods-ThumbsUp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ThumbsUp : NSObject
+@end
+@implementation PodsDummy_Pods_ThumbsUp
+@end
